9ff285caf811e64aaff03e2a4688816cbb446799
priyabratadas <pdas544@gmail.com>
2026-04-22 15:04:46 +0530

    add html and css for quiz game.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>My Quiz Game</h1>
+    <h2>Question: <span>Python is a programming language.</span></h2>
+    
+    <div class="options">
+      <input type="radio" name="options" id="" value="True">
+      <label for="">True</label>
+      <input type="radio" name="options" id="" value="False">
+      <label for="">False</label>
+      
+    </div>
+    <button class="btn-submit">Submit</button>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
@@ -22,5 +32,46 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#app div{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 750px;
+  margin: 50px auto;
+}
+
+#app div h2{
+  font-size: 24px;
+  margin-top: 20px;
+  border: 2px solid blue;
+  border-radius: 12px;
+  height: 50px;
+  width: 100%;
+  padding: 10px 12px;
+}
+
+#app span{
+  color: blue;
+}
+
+#app div .options{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 30px;
+}
+
+.btn-submit{
+  padding: 10px 20px;
+  background-color: blue;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
 }
 </style>
